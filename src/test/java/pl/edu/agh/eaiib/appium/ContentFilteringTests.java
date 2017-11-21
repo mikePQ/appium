a052cbcb97629a6fc7e20141ad6b85eae5bbb28d
Michał Perlak <michalperlak@windowslive.com>
2017-11-21 21:02:20 +0100

    Scenarios skeleton

---
+++ src/test/java/pl/edu/agh/eaiib/appium/ContentFilteringTests.java
@@ -0,0 +1,19 @@
+package pl.edu.agh.eaiib.appium;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ContentFilteringTests {
+
+    @Test
+    @DisplayName("View posts with highest rate")
+    void testContentFiltering1() {
+
+    }
+
+    @Test
+    @DisplayName("View most recent posts")
+    void testContentFiltering2() {
+
+    }
+}
---
+++ src/test/java/pl/edu/agh/eaiib/appium/SettingsTests.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.eaiib.appium;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SettingsTests {
+
+    @Test
+    @DisplayName("User can select nightly mode")
+    void testSettings1() {
+
+    }
+
+    @Test
+    @DisplayName("User can select daily mode")
+    void testSettings2() {
+
+    }
+
+    @Test
+    @DisplayName("User can select default filtering")
+    void testSettings3() {
+
+    }
+}

df4b4fdf8bec6ec4274ce57b52fa665b7f2dc912
Michał Perlak <michalperlak@windowslive.com>
2017-11-22 21:05:56 +0100

    Scenario for content filtering

--- src/test/java/pl/edu/agh/eaiib/appium/ContentFilteringTests.java
+++ src/test/java/pl/edu/agh/eaiib/appium/ContentFilteringTests.java
@@ -1,19 +1,59 @@
 package pl.edu.agh.eaiib.appium;
 
+import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pl.edu.agh.eaiib.appium.junit.AppiumExtension;
 
+@ExtendWith(AppiumExtension.class)
 public class ContentFilteringTests {
 
     @Test
     @DisplayName("View posts with highest rate")
-    void testContentFiltering1() {
+    void testContentFiltering1(AndroidDriver androidDriver) {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 60);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(FILTER_SETTINGS_BUTTON_ID)));
 
+        WebElement filterSettingsButton = androidDriver.findElementByAccessibilityId(FILTER_SETTINGS_BUTTON_ID);
+        filterSettingsButton.click();
+
+        wait = new WebDriverWait(androidDriver, 60);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(HIGHEST_RATED_POSTS_OPTION_PATH)));
+
+        WebElement highestRatedOption = androidDriver.findElementByXPath(HIGHEST_RATED_POSTS_OPTION_PATH);
+        highestRatedOption.click();
+
+        wait = new WebDriverWait(androidDriver, 60);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(HIGHEST_RATED_POSTS_VIEW_LABEL_XPATH)));
     }
 
     @Test
     @DisplayName("View most recent posts")
-    void testContentFiltering2() {
+    void testContentFiltering2(AndroidDriver androidDriver) {
+        WebDriverWait wait = new WebDriverWait(androidDriver, 60);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id(FILTER_SETTINGS_BUTTON_ID)));
 
+        WebElement filterSettingsButton = androidDriver.findElementByAccessibilityId(FILTER_SETTINGS_BUTTON_ID);
+        filterSettingsButton.click();
+
+        wait = new WebDriverWait(androidDriver, 60);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(MOST_RECENT_POSTS_OPTION_XPATH)));
+
+        WebElement mostRecentOption = androidDriver.findElementByXPath(MOST_RECENT_POSTS_OPTION_XPATH);
+        mostRecentOption.click();
+
+        wait = new WebDriverWait(androidDriver, 120);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(MOST_RECENT_POSTS_VIEW_LABEL_XPATH)));
     }
+
+    private static final String FILTER_SETTINGS_BUTTON_ID = "More options";
+    private static final String MOST_RECENT_POSTS_OPTION_XPATH = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[4]/android.widget.RelativeLayout/android.widget.TextView";
+    private static final String HIGHEST_RATED_POSTS_OPTION_PATH = "/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.RelativeLayout/android.widget.TextView";
+    private static final String MOST_RECENT_POSTS_VIEW_LABEL_XPATH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView";
+    private static final String HIGHEST_RATED_POSTS_VIEW_LABEL_XPATH = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView";
 }
